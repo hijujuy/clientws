1c00da2513d067ff326ad39ea33c16535b348bae
Diego <dmchumacero@gmail.com>
2021-02-19 11:17:26 -0300

    class Cliente

---
+++ application/libraries/Cliente.php
@@ -0,0 +1,50 @@
+<?php
+
+class Cliente {
+    
+    /**
+     * $id
+     *
+     * @var int     identificador de registro en base de datos.
+     */
+    protected $id;
+
+    /**
+     * $email
+     *
+     * @var string  email del cliente registrado.
+     */
+    protected $email;
+
+    /**
+     * $cuit
+     *
+     * @var long   cuit del cliente.
+     */
+    protected $cuit;
+
+    /**
+     * $dni
+     *
+     * @var long    dni del cliente.
+     */
+    protected $dni;
+
+    /**
+     * $apellido
+     *
+     * @var string  apellido del cliente.
+     */
+    protected $apellido;
+
+    /**
+     * $nombre
+     *
+     * @var string  nombre del cliente registrado.
+     */
+    protected $nombre;
+    
+
+}
+
+?>
